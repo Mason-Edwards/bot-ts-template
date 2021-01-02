7fdcc0640aeff28088057e32ca75619b1789b443
Mason <edwardsmason2000@gmail.com>
2021-01-02 04:44:28 +0000

    Update bot.ts

--- src/bot.ts
+++ src/bot.ts
@@ -1,5 +1,4 @@
 import { token, owners } from "./config";
-import Botclient from "./client/botClient";
 import BotClient from "./client/botClient";
 
 const client: BotClient = new BotClient({token, owners});
